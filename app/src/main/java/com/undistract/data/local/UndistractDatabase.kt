1a249d82f46aae33e2c5309f7c88ec62de14158b
emackinnon1 <emackinnon100390@gmail.com>
2025-06-15 10:46:57 -0600

    Database set up with Profiles

---
+++ app/src/main/java/com/undistract/data/local/UndistractDatabase.kt
@@ -0,0 +1,24 @@
+package com.undistract.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.undistract.data.daos.ProfileDao
+import com.undistract.data.room.ProfileEntity
+
+@Database(
+    entities = [
+        ProfileEntity::class,
+//        NfcTagEntity::class,
+//        AppInfoEntity::class,
+//        UndistractedStatsEntity::class,
+//        EmergencyUsageEntity::class
+    ],
+    version = 1
+)
+abstract class UndistractDatabase : RoomDatabase() {
+    abstract fun profileDao(): ProfileDao
+//    abstract fun nfcTagDao(): NfcTagDao
+//    abstract fun appInfoDao(): AppInfoDao
+//    abstract fun statsDao(): UndistractedStatsDao
+//    abstract fun emergencyUsageDao(): EmergencyUsageDao
+}
