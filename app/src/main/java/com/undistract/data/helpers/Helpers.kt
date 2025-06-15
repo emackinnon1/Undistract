f31552d8e5c5a92e1bca515de1c33a24c93e73de
emackinnon1 <emackinnon100390@gmail.com>
2025-06-15 13:07:23 -0600

    Add a few helper methods

--- app/src/main/java/com/undistract/data/helpers/Helpers.kt
+++ app/src/main/java/com/undistract/data/helpers/Helpers.kt
@@ -1,6 +1,12 @@
 package com.undistract.data.helpers
 
 import androidx.room.TypeConverter
+import android.content.Context
+import android.graphics.drawable.Drawable
+import androidx.core.content.res.ResourcesCompat
+import com.undistract.data.entities.AppInfoEntity
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 
 class StringListConverter {
     @TypeConverter
@@ -9,4 +15,14 @@ class StringListConverter {
     @TypeConverter
     fun toList(data: String): List<String> =
         if (data.isEmpty()) emptyList() else data.split(",")
+}
+
+fun AppInfoEntity.loadIcon(context: Context): Drawable? {
+    val resId = context.resources.getIdentifier(iconResName, "drawable", context.packageName)
+    return if (resId != 0) ResourcesCompat.getDrawable(context.resources, resId, null) else null
+}
+
+fun AppInfoEntity.loadBitmap(context: Context): Bitmap? {
+    val drawable = loadIcon(context)
+    return (drawable as? BitmapDrawable)?.bitmap
 }
